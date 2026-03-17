b59046f83c30c7dc44b8d688e3cf34d45ae36d27
Emmanuel Sofuwa <102985819+SOFSV12@users.noreply.github.com>
2026-03-17 08:32:16 +0100

    polymorphic relationship one to one

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,10 @@ Route::get('/', function () {
     //fetch post image
     $post = Post::find(1);
     $image = $post->image;
+    
 
     //fecth parent model, will return either a post or a user instance
     $image = Image::find(1);
     $imageable = $image->imageable;
-
+    dd($imageable);
 });
